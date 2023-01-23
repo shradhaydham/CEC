5f4aebb38b10c68289a37ffdcc4f8938d16ae343
SHRADHAY DHAM <shradhay.dham@gmail.com>
2023-01-23 23:19:35 +0530

    Add files via upload

--- recursion/lcs.cpp
+++ recursion/lcs.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int lcs(string &s,string &t,int i,int j)
+{
+    if(i==s.size() || j==t.size())
+    return 0;
+    if(s[i]==t[j])
+    return 1+lcs(s,t,i+1,j+1);
+    else
+    return max(lcs(s,t,i,j+1),lcs(s,t,i+1,j));
+}
+int main()
+{
+    string s="";
+    string t="";
+    cin>>s>>t;
+    int ans=lcs(s,t,0,0);
+    if(ans==-1)
+    cout<<"No subsequence";
+    else 
+    cout<<"LCS "<<ans;
+    return 0;
+}
