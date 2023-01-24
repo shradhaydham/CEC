0aa92afd964b1d73225684812d85ea5af77a3e1b
SHRADHAY DHAM <shradhay.dham@gmail.com>
2023-01-24 21:11:46 +0530

    Add files via upload

---
+++ ExpressionConversion/infix_to_postfix.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int priority(char c){
+    if(c=='^')return 3;
+    else if(c=='*' || c=='/')return 2;
+    else if(c=='+' || c=='-')return 1;
+    else return -1;
+}
+string convert(string &s){
+    string ans;
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
+        ans+=s[i];
+        else if(s[i]=='(')
+        st.push('(');
+        else if(s[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                char c=st.top();
+                st.pop();
+                ans+=c;
+            }
+            st.pop();
+        }
+        else{
+            while(!st.empty() && priority(s[i])<=priority(st.top())){
+                char c=st.top();
+                st.pop();
+                ans+=c;
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()){
+        char c=st.top();
+        st.pop();
+        ans+=c;
+    }
+    return ans;
+}
+int main(){
+    string s="";
+    cin>>s;
+    cout<<"Postfix\n"<<convert(s);
+    return 0;
+}
